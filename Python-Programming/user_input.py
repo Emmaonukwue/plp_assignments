a6a1f718fcda24c5210fcc2bb694e78507eaca8b
Design IT <72794319+Emmaonukwue@users.noreply.github.com>
2024-02-24 11:06:08 +0100

    Create user_input.py

---
+++ Python-Programming/user_input.py
@@ -0,0 +1,12 @@
+"""Emmanuel Onukwue's Week 1 Assignment
+User_input.py
+PLP - Python Programming
+"""
+
+name = input('What is your name? ');
+age = input('What is your age? ');
+location = input('Where is your location (where you live)? ');
+print();
+print(f'Hello {name}, you are {age} years old and you live in {location}.');
+
+"The end!"
